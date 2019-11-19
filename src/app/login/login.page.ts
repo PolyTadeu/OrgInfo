e9e06f7db289559e8bb016ec3ccb438741417fe9
PolyTadeu <polytadeu@gmail.com>
2019-11-19 19:59:48 -0200

    home, flow das telas

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import{FormGroup ,FormBuilder,Validators} from '@angular/forms';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -9,12 +9,18 @@ import{FormGroup ,FormBuilder,Validators} from '@angular/forms';
 export class LoginPage implements OnInit {
 
   loginForm: FormGroup;
-  constructor( public formBuilder: FormBuilder) { 
+  constructor( public formBuilder: FormBuilder, private router: Router) { 
     this.loginForm = this.formBuilder.group({
   		email: [null, [Validators.required, Validators.email]],
   		password: [null, [Validators.required, Validators.minLength(8)]],
   	});
   }
+
+  irParaCadastro(){
+    this.router.navigateByUrl('/cadastro');
+
+  }
+
 
   ngOnInit() {
   }
